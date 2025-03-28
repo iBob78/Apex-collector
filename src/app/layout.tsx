f1e0b4810126254114fed721cbfa2478813c6f7f
iBob78 <66393230+iBob78@users.noreply.github.com>
2025-03-28 13:12:25 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="fr">
-            <head>
-                <title>Apex Collector</title>
-            </head>
             <body>
                 <header>
                     <h1>Apex Collector</h1>
