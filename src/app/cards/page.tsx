b6117c99258c76179119802bb24981c6a747a4eb
iBob78 <66393230+iBob78@users.noreply.github.com>
2025-04-01 09:46:27 +0200

    Update page.tsx

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
-import Card from "@/components/Card";
+import Card from "../../components/Card";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
